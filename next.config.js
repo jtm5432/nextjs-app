9af3e49ba5e62d04996e933e59c804acc762b4ca
unknown <jtm5432@naver.com>
2023-09-20 21:48:38 +0900

    jest 설정, customhook 등등 추가

--- next.config.js
+++ next.config.js
@@ -1,10 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode : true,
+    reactStrictMode: true,
     compiler: {
-        styledComponents: true,
+      styledComponents: true,
+    },
+    webpack: (config, { dev, isServer }) => {
+      // 프로덕션 환경에서만 실행
+      if (!dev) {
+        config.module.rules.push({
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              [
+                'babel-plugin-react-remove-properties', 
+                { properties: ['data-testid'] }
+              ]
+            ],
+            compact: false,
+          }
+        });
+      }
+  
+      return config;
     }
-
-}
-
-module.exports = nextConfig
+  };
+  
+  module.exports = nextConfig;
+  
